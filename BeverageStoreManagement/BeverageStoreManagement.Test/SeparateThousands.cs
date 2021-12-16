e0b3c10d46c612d35e1203e5140e34a849c8a005
thanhphat219 <phat01283249260@gmail.com>
2021-12-16 22:02:51 +0700

    Update

---
+++ BeverageStoreManagement/BeverageStoreManagement.Test/SeparateThousands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using BeverageStoreManagement.ViewModels;
+namespace BeverageStoreManagement.Test
+{
+    [TestFixture]
+    class SeparateThousands
+    {
+        BaseViewModel baseViewModel = new BaseViewModel();
+
+        [TestCase("123456789","123,456,789")]
+        [TestCase("", "")]
+        public void TestCase(string input, string output)
+        {
+            Assert.AreEqual(output, baseViewModel.SeparateThousands(input));
+        }
+    }
+}
