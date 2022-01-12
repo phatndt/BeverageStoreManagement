49a4aa46d144312098d77d607928e7fd18b89f49
thanhphat219 <phat01283249260@gmail.com>
2022-01-12 07:17:50 +0700

    update

--- BeverageStoreManagement/BeverageStoreManagement/ViewModels/ImportBillViewModel.cs
+++ BeverageStoreManagement/BeverageStoreManagement/ViewModels/ImportBillViewModel.cs
@@ -125,7 +125,7 @@ namespace BeverageStoreManagement.ViewModels
             }
         }
 
-        private void LoadPendingRequest(MainWindow parameter)
+        public void LoadPendingRequest(MainWindow parameter)
         {
             this.mainWindow = parameter;
             parameter.stkImportMaterial.Children.Clear();
@@ -144,7 +144,7 @@ namespace BeverageStoreManagement.ViewModels
             }    
         }
 
-        private void LoadImportedRequest(MainWindow parameter)
+        public void LoadImportedRequest(MainWindow parameter)
         {
             parameter.stkImportedMaterial.Children.Clear();
 
